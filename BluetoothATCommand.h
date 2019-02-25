f05dd3660414122308cb3e7bec61e02b069d0020
Dreamwalker <aristojeff@gmail.com>
2019-02-25 12:39:50 +0900

    Update BluetoothATCommand.h

--- BluetoothATCommand.h
+++ BluetoothATCommand.h
@@ -6,22 +6,22 @@
 #define default_pulse_ms 100
 #define default_timeout_ms 1000
 
+class BluetoothATCommand
+{
+public:
+  BluetoothATCommand(int8_t rst, bool newline = false);
+  begin(Stream &port);
+  void reset(uint8_t pulse, uint16_t duration = default_pulse_ms);
+  bool sendBlindCommand(char *command);
+  bool sendCommand(char *command, uint16_t timeout = default_timeout_ms);
+  bool sendCommand(char *command, char *reply, uint16_t timeout = default_timeout_ms);
 
-class BluetoothATCommand {
-  public:
-    void BluetoothATCommand(int8_t rst, bool newline = false);
-    void begin(Stream &port);
-    void reset(uint8_t pulse, uint16_t duration = default_pulse_ms);
-    bool sendBlindCommand(char *command);
-    bool sendCommand(char *command, uint16_t timeout = default_timeout_ms);
-    bool sendCommand(char *command, char *reply, uint16_t timeout = default_timeout_ms);
-    
-  private:
-    int8_t _rst;
-    bool newline = false;
-    char replybuffer[255];
-    unsigned long timer;
-    Stream *mySerial;
+private:
+  int8_t _rst;
+  bool newline = false;
+  char replybuffer[255];
+  unsigned long timer;
+  Stream *mySerial;
 };
 
 #endif
